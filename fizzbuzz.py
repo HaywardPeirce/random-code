421cc8ca466189a83b7fe3d0e058a8a5a5367c75
HaywardPeirce <hpeirce@gmail.com>
2017-08-03 03:55:17 +0000

    HP:new methods and comments

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,52 +1,105 @@
-import sys
+import sys, argparse
 
+#setup fallback values
 count = 1
 countLimit = 100
-fBMethod = "ifelse"
+method = "ifelse"
 
-if sys.argv[1]:
-    fBMethod = sys.argv[1]
+#parse input arguments -m, method, -l, limit
+parser = argparse.ArgumentParser()
+parser.add_argument('-m','--method', help='Which FizzBuzz generation method to use')
+parser.add_argument('-l','--limit',help='How many numbers to run FizzBuzz for', type=int)
+args = parser.parse_args()
 
-def ifElseMethod(num):
-    #using if else statements
-    #print("using the ifelse method")
+if args.method: method = args.method
+if args.limit: countLimit = args.limit
+
+#function to generate FizzBuzz using nested if and else statements with prints
+def ifElseMethod(count):
     
-    if num % 3 == 0:
+    #loop through all the numbers to be checked up to, and including the limit
+    while count <= countLimit:
+    
+        if count % 3 == 0:
+            if count % 5 == 0:
+                #if the countber is a multiple of both 3 and 5
+                print("FizzBuzz")
+            #if the countber is only a multiple of 3
+            else: print("Fizz")
+        #if the countber wasn't a multiple of 3 but is a multiple of 5
+        elif count % 5 == 0:
+            print ("Buzz")
+        #if the countber is neither a multiple of 3 or 5
+        else: print(count)
+        
+        count += 1
+
+#function to generate FizzBuzz using just if statements and concatonating results for each number before printing
+def stringConCatMethod(count):
+    
+    #loop through all the numbers to be checked up to, and including the limit
+    while count <= countLimit:
+    
+        string = ""
+        
+        if count % 3 == 0:
+            string += "Fizz"
+        if count % 5 == 0:
+            string += "Buzz"
+        if count % 3 != 0 and count % 5 != 0: string += str(count)
+        
+        print(string)
+        
+        count += 1
+    
+def recursiveMethod():
+    
+    string = recursivePrint(0)
+    
+    #remove the leading "\n" to be character-accurate
+    print(string.lstrip())
+    
+def recursivePrint(num):
+    
+    returnedString = ""
+    
+    num += 1
+    
+    #if this is not the last number to be checked
+    if num <= countLimit:
+        
+        returnedString = recursivePrint(num)
+    
+        #past here means we have reached the bottom and are moving back up
+            
+        #initialize the temp string for each function call
+        tempString = ""
+        thisString = ""
+        
+        if num % 3 == 0:
+            thisString += "\nFizz"
         if num % 5 == 0:
-            #if the number is a multiple of both 3 and 5
-            print("FizzBuzz")
-        #if the number is only a multiple of 3
-        else: print("Fizz")
-    #if the number wasn't a multiple of 3 but is a multiple of 5
-    elif num % 3 == 0:
-        print ("Buzz")
-    #if the number is neither a multiple of 3 or 5
-    else: print(num)
+            if num % 3 == 0:
+                thisString += "Buzz"
+            if num % 3 != 0:
+                thisString += "\nBuzz"
+        if num % 3 != 0 and num % 5 != 0: thisString += ('\n' + str(num))
+        
+        
+        tempString = thisString
+        
+        #check for empty returnString on first run through
+        if returnedString: tempString += returnedString
+        
+        #pass the string back up to be added onto
+        return tempString
+    
+if method == "ifelse": ifElseMethod(count)
+elif method == "concat": stringConCatMethod(count)
+elif method == "recursive": recursiveMethod()
+else: print("No function found")
 
-def stringConCatMethod(num):
-    #check each criteria, concatonate onto end of string 
-    #print("using the concat method")
-    
-    string = ""
-    
-    if num % 3 == 0:
-        string += "Fizz"
-    if num % 5 == 0:
-        string += "Buzz"
-    if num % 3 != 0 and num % 5 != 0: string += str(num)
-    
-    print(string)
 
-def selectFBMethod(num, fBMethod):
-    if fBMethod == "ifelse": ifElseMethod(num)
-    elif fBMethod == "concat": stringConCatMethod(num)
-    else: print("No function found")
-
-while count <= countLimit:
-    #print("Number: ", count)
-    #print(fBMethod)
-    #select method to check for FizzBuzz
-    selectFBMethod(count, fBMethod)
     
-    count += 1
+    
     
