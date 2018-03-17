92d06da9d46adbe6488e158ab77f2755ec7bc934
HaywardPeirce <hpeirce@gmail.com>
2018-03-17 21:20:38 +0000

    HP: add setup file

---
+++ setup.py
@@ -0,0 +1,8 @@
+from distutils.core import setup
+
+setup(name='random-code',
+      version='0.1',
+      description='Package of assorted small code projects',
+      url='https://github.com/HaywardPeirce/random-code',
+      py_modules = ['collatzConjecture', 'fizzbuzz']
+     )
