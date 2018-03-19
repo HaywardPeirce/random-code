adf6569fe99ae32ac3c5d3513d200535f3d54990
HaywardPeirce <hpeirce@gmail.com>
2018-03-19 00:12:56 +0000

    HP: update version number

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from distutils.core import setup
 
 setup(name='random-code',
-      version='0.1',
+      version='0.2',
       description='Package of assorted small code projects',
       url='https://github.com/HaywardPeirce/random-code',
       py_modules = ['collatzConjecture', 'fizzbuzz']
