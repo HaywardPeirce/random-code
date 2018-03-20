81f5c86ced8bc6362761381d35265f8c4b851a57
HaywardPeirce <hpeirce@gmail.com>
2018-03-20 02:39:31 +0000

    HP: update setuptools

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='random-code',
-      version='0.3.2',
+      version='0.3.4',
       description='Package of assorted small code projects',
       url='https://github.com/HaywardPeirce/random-code',
       py_modules = ['collatzConjecture', 'fizzbuzz']
