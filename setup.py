63d57b75d35ce1a3505ff9d2d3dfb49f5909900b
HaywardPeirce <hpeirce@gmail.com>
2018-03-20 02:17:19 +0000

    HP: remove mention of hoffmanCoding

--- setup.py
+++ setup.py
@@ -4,5 +4,5 @@ setup(name='random-code',
       version='0.3.2',
       description='Package of assorted small code projects',
       url='https://github.com/HaywardPeirce/random-code',
-      py_modules = ['collatzConjecture', 'fizzbuzz', 'hoffmanCoding']
+      py_modules = ['collatzConjecture', 'fizzbuzz']
      )
