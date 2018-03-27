2ea2ef0863f0120db16fe30a7ee749aa88998b96
HaywardPeirce <hpeirce@gmail.com>
2018-03-27 03:21:33 +0000

    HP: migrade to setup tools again, and add requirements

--- setup.py
+++ setup.py
@@ -1,8 +1,9 @@
-from distutils.core import setup
+from setuptools import setup
 
 setup(name='random-code',
-      version='0.4',
+      version='0.4.1',
       description='Package of assorted small code projects',
+      install_requires=['setuptools>=4.0.1'],
       url='https://github.com/HaywardPeirce/random-code',
       py_modules = ['collatzConjecture', 'fizzbuzz']
      )
